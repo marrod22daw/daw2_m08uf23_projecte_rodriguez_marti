cb0286165937e458fd292492eab39d8bf9b15c78
marrod22daw <marrod22daw@proton.me>
2023-04-21 19:29:27 +0200

    Segon commit del projecte zendlap

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 use Laminas\Ldap\Ldap;
+ini_set('display_errors',0);
+if ($_GET['usr'] && $_GET['ou']){
 $domini = 'dc=fjeclot,dc=net';
 $opcions = [
  'host' => 'zend-maroma.fjeclot.net',
@@ -12,9 +14,27 @@ $opcions = [
  ];
 $ldap = new Ldap($opcions);
 $ldap->bind();
-$usuari=$ldap->getEntry('uid=sysdev,ou=desenvolupadors,dc=fjeclot,dc=net');
+$entrada='uid='.$_GET['usr'].',ou='.$_GET['ou'].',dc=fjeclot,dc=net';
+$usuari=$ldap->getEntry($entrada);
 echo "<b><u>".$usuari["dn"]."</b></u><br>";
 foreach ($usuari as $atribut => $dada) {
  if ($atribut != "dn") echo $atribut.": ".$dada[0].'<br>';
 }
+}
 ?>
+<html>
+<head>
+<title>
+MOSTRANT DADES D'USUARIS DE LA BASE DE DADES LDAP
+</title>
+</head>
+<body>
+<h2>Formulari de selecció d'usuari</h2>
+<form action="http://zend-maroma.fjeclot.net/zendldap" method="GET">
+Unitat organitzativa: <input type="text" name="ou"><br>
+Usuari: <input type="text" name="usr"><br>
+<input type="submit"/>
+<input type="reset"/>
+</form>
+</body>
+</html>
